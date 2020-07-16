c3de405d7a3ed8adc84e469d64c982bc591c0ec7
ivan.katic@live.com <ivan.katic@live.com>
2020-07-17 00:30:53 +0200

    Updates

---
+++ precache-manifest.e1fed58b42bd63e7d4b2275d6f546753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a408e1892835b4063deaf616b0ae10",
+    "url": "/power-forecast/index.html"
+  },
+  {
+    "revision": "80eec8fa18edef1f6454",
+    "url": "/power-forecast/static/css/2.6eff3e32.chunk.css"
+  },
+  {
+    "revision": "0a666574a66de244e708",
+    "url": "/power-forecast/static/css/main.3bb19eab.chunk.css"
+  },
+  {
+    "revision": "80eec8fa18edef1f6454",
+    "url": "/power-forecast/static/js/2.c2b880fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/power-forecast/static/js/2.c2b880fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a666574a66de244e708",
+    "url": "/power-forecast/static/js/main.83848017.chunk.js"
+  },
+  {
+    "revision": "d19018c0965331acb106",
+    "url": "/power-forecast/static/js/runtime-main.b45bccc8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/power-forecast/precache-manifest.e1fed58b42bd63e7d4b2275d6f546753.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/power-forecast/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
